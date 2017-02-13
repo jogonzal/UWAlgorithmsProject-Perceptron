0aacb808617f7ac1af42e1f119ba05f42bc858be
andresz <andresz@microsoft.com>
2017-02-12 19:28:57 -0800

    Adding thermal perceptron

---
+++ PerceptronVariations/PerceptronVariations/Perceptrons/ThermalSimplePerceptron.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using PerceptronVariations.Interfaces;
+using PerceptronVariations.Plotting;
+
+namespace PerceptronVariations.Perceptrons
+{
+	public class ThermalSimplePerceptron : SimplePerceptron
+	{
+		public double Temperature { get; }
+		public ThermalSimplePerceptron(ITransferFunction transferFunction, int maxEpochs, double learningRate, double temperature) : base(transferFunction, maxEpochs, learningRate)
+		{
+			Temperature = temperature;
+		}
+
+		public override double Normalize(double[] weights, Point currentPoint)
+		{
+			double sum = ObtainSum(weights, currentPoint);
+
+			return Math.Pow(Math.E, -(Math.Abs(sum) / Temperature));
+		}
+	}
+}
